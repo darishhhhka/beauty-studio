ec32e8801dcc12da3f0c0e8af0eb2385159ece24
tarusovadarina6 <tarusovadarina6@gmail.com>
2025-10-28 02:39:25 +0300

    upd: Page, перенесен фон с HomeScreen на Page

--- src/components/UI/page/Page.tsx
+++ src/components/UI/page/Page.tsx
@@ -1,13 +1,26 @@
 import type { JSX } from "react";
 import "./Page.css";
+import { Footer } from "../../Footer/Footer";
+import { Header } from "../../Header/Header";
 
 type Props = { content: JSX.Element };
+const contact = [
+  "+7 (812) 123-45-67",
+  "+7 (911) 123-45-67",
+  "Новоостровский проспект, дом 36 лит. С",
+];
+
+const mode = ["C 10:00 до 21:00 (Пн-Пт)", "С 11:00 до 20:00 (Сб-Вс)"];
 export const Page = ({ content }: Props) => {
   return (
     <div className="page">
-      <div>Header</div>
-      {content}
-      <div>Footer</div>
+      <div className="page-bg">
+        <div className="page-container">
+          <Header />
+          {content}
+          <Footer infoContacts={contact} infoMode={mode} />
+        </div>
+      </div>
     </div>
   );
 };
